681e676d4573ea2d18a61c781e65bd95b74c84f8
Joosep Parts <joosep.parts@gmail.com>
2022-12-05 21:17:25 +0200

    chart stuff

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -165,6 +165,7 @@ export class UserService {
 
   isShowFooter() {
     let currentUrl = this.router.url;
+    console.warn(['home', 'stats', 'results'].includes(currentUrl.replace(/[^a-zA-Z0-9 ]/g, '')))
     return !['home', 'stats', 'results'].includes(currentUrl.replace(/[^a-zA-Z0-9 ]/g, ''));
   }
 
